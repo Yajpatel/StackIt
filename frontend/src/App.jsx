a4ac7a9a20185351d5a85b8c910381f16794a582
Om Patel <202273921+ompatel100@users.noreply.github.com>
2025-07-12 17:36:10 +0530

    fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/UniversalComponent/Navbar";
@@ -8,10 +7,12 @@ import HomePage from "./pages/HomePage";
 import AskQuestion from "./pages/AskQuestion";
 import AuthPage from "./pages/AuthPage";
 import QuestionDetail from "./pages/QuestionDetail";
+import Editor from "./components/Editor/Editor.jsx"
 import "./App.css";
 
 function App() {
   return (
+  // <Editor content={""}></Editor>
     <AuthProvider>
       <Router>
         <div className="App">
@@ -27,24 +28,6 @@ function App() {
           <Footer />
         </div>
       </Router>
-=======
-import Editor from "./components/Editor/Editor.jsx"
-
-function App() {
-  return (
-    // <Editor content={""}></Editor>
-    <BrowserRouter>
-    <AuthProvider>
-      <div className="App">
-        <Routes>
-         <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} />
-          <Route path="/AskQuestion" element={<AskQuestion />} />
-          <Route path="/answer/:id" element={<AnswerPage />} />
-        </Routes>
-      </div>
-      <ToastContainer />
->>>>>>> 1093951f4b0633c233adf45edd51a822823a8111
     </AuthProvider>
   );
 }
--- frontend/src/components/Editor/Editor.jsx
+++ frontend/src/components/Editor/Editor.jsx
@@ -1,7 +1,7 @@
 import { SimpleEditor } from "../../../@/components/tiptap-templates/simple/simple-editor";
 import React, { useState } from "react";
 
-export default function Editor({content}) {
+export default function Editor({content = ""}) {
   const [editor, setEditor] = useState(null);
 
   const handleGetHTML = () => {
